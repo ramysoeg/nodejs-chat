ebf381aa5aed4a17b360db08f2f9cd2d2e502ada
ramysoeg <ramysoeg@gmail.com>
2022-11-04 15:17:24 -0300

    first-test

---
+++ app.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import path from 'path';
+import http from 'http';
+
+const cors = require("cors");
+const app =  express();
+
+app.set('port_http', process.env.port_http || 8080);
+app.set('port_ws', process.env.port_ws || 3001);
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public/assets')));
+app.set('views', path.join(__dirname, 'public/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+const server = http.createServer(app, {
+    cors: {
+        origin: "http://localhost:8080"
+    }
+}).listen(app.get('port_http'), function() {
+    console.log("Express server listen on port ".concat(app.get('port_http')));
+});
+
+const io = require('socket.io')(server);
+
+const messages = {
+    _all: [],
+    add(m) {
+        this._all.push(m);
+    },
+    all() {
+        return this._all;
+    }
+};
+
+const audience = {
+    _all: [],
+    _NewAudience(a){
+        return {
+            SendMessage(target, chanel, message) {
+                target.emit(chanel, message);
+            }
+        }
+    },
+    new(a) {
+        this._all.push(this._NewAudience(a));
+    }
+};
+
+function IoConnection(person) {
+    console.log(`New connection socket.id: ${person.id}`);
+    person.emit('socket-id', {socketId:person.id});
+    const previousMessages = messages.all();
+    console.log({'previous-message':previousMessages});
+    person.emit('previous-message', previousMessages);
+}
+
+io.on('connection', (socket) => {
+    IoConnection(socket);
+
+    socket.on('message', (mesage) => {
+        console.log(mesage);
+        messages.add(mesage);
+        socket.broadcast.emit('received-message', mesage);
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
---
+++ public/assets/js/script.js
@@ -0,0 +1,65 @@
+const nicknamePlace = $('#nickname');
+const messagePlace = $('#message');
+const message = $('#chat');
+
+let author = null;
+
+var socket = io('http://localhost:8080');
+
+function RenderMessage(m) {
+    const msgLine = `<div><strong>${m.author}:&nbsp;</strong>${m.message}</div>`;
+    $('#messages').append(msgLine);
+}
+
+function SubmitMessage(m) {
+    socket.emit('message', m);
+
+    RenderMessage(m);
+}
+
+function PreSend() {
+    if (author.length > 0 && message.val().length > 0) {
+        const messageObject = {
+            author,
+            message: message.val()
+        };
+
+        SubmitMessage(messageObject);
+        message.val('');
+    }
+    return false;
+}
+
+$('#btn-nickname').on('click', () => {
+    if (nicknamePlace.val().length > 0) {
+        author = nicknamePlace.val();
+        socket.emit('set-nickname', {
+            data: author
+        });
+    }
+});
+
+socket.on('socket-id', data => {
+    console.log(data);
+});
+
+socket.on('received-message', (data) => {
+    console.log('received-message');
+    RenderMessage(data);
+});
+
+socket.on('previous-message', (data) => {
+    for (const i in data) {
+        RenderMessage(data[i]);
+    }
+});
+
+$('#btn-chat').on('click', () => {
+    PreSend();
+});
+
+$('#chat').keyup((e) => {
+    if(e.keyCode == 13) {
+        PreSend();
+    }
+});
